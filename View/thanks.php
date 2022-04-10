aebb047d450bc95f3f5a851f6f3fb492fb87d4d5
Tuyết Trinh <88887819+letrinh2203@users.noreply.github.com>
2022-04-10 21:01:30 +0700

    Update thanks.php

--- View/thanks.php
+++ View/thanks.php
@@ -2,8 +2,8 @@
         <h3 style="margin-top: 100px ;">
 Cảm ơn bạn đã mua hàng!<br>
 Chúng tôi sẽ gửi email thông tin đơn hàng cho bạn sau khi xác nhận đơn hàng! <br>
-Nếu có bất kì ý kiến gì về sản phẩm<br>
-Xin vui lòng liên hệ với chúng tôi qua số điện thoại +84 392584784 <br>
+
+Mọi thắc mắc xin vui lòng liên hệ với chúng tôi qua số điện thoại +84 392584784 <br>
 
 <?php echo '<a href="/DA_CoffeeShop/index.php">Trang chủ</a>';?>
 
